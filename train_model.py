b514f36f61a4fedea5fa013b585024d48ad12549
Shubham Verma <64227183+Quantumo0o@users.noreply.github.com>
2025-03-30 14:14:05 +0530

    Update train_model.py

--- train_model.py
+++ train_model.py
@@ -10,7 +10,7 @@ from sklearn.metrics import mean_squared_error, r2_score
 import joblib  # For saving the scaler
 
 # Load dataset
-df = pd.read_csv('car_purchasing.csv')
+df = pd.read_csv('car_purchasing.csv', encoding='latin-1')
 
 # Drop unnecessary columns
 df = df.drop(['customer name', 'customer e-mail', 'country'], axis=1)
@@ -24,7 +24,7 @@ scaler = StandardScaler()
 X_scaled = scaler.fit_transform(X)
 
 # Save the scaler for future use
-joblib.dump(scaler, 'scaler.pkl')
+joblib.dump(scaler, 'car_scaler.pkl')
 
 # Split data into training and testing sets
 X_train, X_test, y_train, y_test = train_test_split(X_scaled, y, test_size=0.2, random_state=42)
